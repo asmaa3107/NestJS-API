90e9dc1cb3816f7c94c42b31fe0602329e99990d
Asmaa Ahmed El swaify <asmaa3107@gmail.com>
2022-08-20 04:29:24 +0200

    delete and patch update statament"

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,7 +1,14 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
-import { title } from 'process';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
 import { CreateTaskeDto } from './dtos/create-task-dto';
-import { Task } from './task.model';
+import { Task, TaskStatus } from './task.model';
 import { TasksService } from './tasks.service';
 @Controller('tasks')
 export class TasksController {
@@ -12,8 +19,25 @@ export class TasksController {
     return this.taskService.getAllTasks();
   }
 
+  @Get('/:id')
+  getTaskById(@Param('id') id: string): Task {
+    return this.taskService.getTaskById(id);
+  }
+  @Delete('/:id')
+  deleteTaskById(@Param('id') id: string): void {
+    this.taskService.deleteTaskById(id);
+  }
+
   @Post()
-  testPostRequest(@Body() createtaskTod: CreateTaskeDto): Task {
+  createTaskRequest(@Body() createtaskTod: CreateTaskeDto): Task {
     return this.taskService.createTask(createtaskTod);
+  }
+
+  @Patch('/:id/status')
+  updateTask(
+    @Param('id') id: string,
+    @Body('status') status: TaskStatus,
+  ): Task {
+    return this.taskService.updateTask(id, status);
   }
 }
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,5 +1,4 @@
 import { Injectable } from '@nestjs/common';
-import { title } from 'process';
 import { v4 as uuid } from 'uuid';
 import { CreateTaskeDto } from './dtos/create-task-dto';
 import { Task, TaskStatus } from './task.model';
@@ -13,6 +12,14 @@ export class TasksService {
   }
   getTaskById(id: string): Task {
     return this.tasks.find((task) => task.id === id);
+  }
+  deleteTaskById(id: string): void {
+    this.tasks = this.tasks.filter((task) => task.id !== id);
+  }
+  updateTask(id: string, status: TaskStatus) {
+    const task = this.getTaskById(id);
+    task.status = status;
+    return task;
   }
   //auto generate id using uuid pakage
   createTask(createTaskdto: CreateTaskeDto): Task {
