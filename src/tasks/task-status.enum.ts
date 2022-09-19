6fcd5412fd24f6a9da1b13c137e8bf6e413228ff
unknown <asmaa3107@gmail.com>
2022-09-19 03:53:05 +0200

    MP-5
    test puh with task name to link git

--- src/tasks/task-status.enum.ts
+++ src/tasks/task-status.enum.ts
@@ -1,12 +1,3 @@
-/* eslint-disable prettier/prettier */
-// eslint-disable-next-line @typescript-eslint/no-unused-vars, prettier/prettier
-export interface Task {
-  id: string;
-  title: string;
-  description: string;
-  status: TaskStatus;
-}
-
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 export enum TaskStatus {
   OPEN = 'OPEN',
